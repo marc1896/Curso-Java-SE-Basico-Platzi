6fb33215604ab7f4b73b7fc81f1f8438b8c506b5
marc1896 <marco123107@hotmail.com>
2022-08-21 01:00:42 -0500

    Se agrego una nueva clase Funciones

---
+++ src/Funciones.java
@@ -0,0 +1,13 @@
+public class Funciones {
+    public static void main(String[] args) {
+        int resultado = suma(3,6);
+        System.out.println("El resultado de suma es " + resultado);
+
+    }
+
+    public static int suma(int a, int b){
+        int suma =0;
+        suma = a + b;
+        return suma;
+    }
+}
