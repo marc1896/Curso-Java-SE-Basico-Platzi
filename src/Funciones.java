5c444424c5436054691a630b37ee7011fd1016dc
marc1896 <marco123107@hotmail.com>
2022-08-21 01:15:18 -0500

    Se agrego documentacion con ayuda de javaDoc a la clase Funciones

--- src/Funciones.java
+++ src/Funciones.java
@@ -5,6 +5,13 @@ public class Funciones {
 
     }
 
+    /**
+     *Descripcion: Funcion para realizar la suma entre dos numero y retorna el resultado
+     *
+     * @param a numero 1
+     * @param b numero 2
+     * @return suma Retorna el resultado de la suma de numero 1 mas numero 2
+     * */
     public static int suma(int a, int b){
         int suma =0;
         suma = a + b;
