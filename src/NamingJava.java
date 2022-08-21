173c35f57459052f877447dbdd1e7b5e252c067e
marc1896 <marco123107@hotmail.com>
2022-08-20 22:41:33 -0500

    Se agrego una nueva clase NamingJava, el cual se llevo a cabo el uso de Convension de nombres en java.

---
+++ src/NamingJava.java
@@ -0,0 +1,16 @@
+public class NamingJava {
+    public static void main(String[] args) {
+        int celphone = 33337777;
+        int celPhone = 55553333;
+
+        System.out.println("celphone = " + celphone);
+        System.out.println("celPhone = " + celPhone);
+
+        String $countryName = "Spain";
+        String _backgroundColor = "Green";
+
+        String currency$ = "MXN";
+        String background_color = "Blue";
+        
+    }
+}
