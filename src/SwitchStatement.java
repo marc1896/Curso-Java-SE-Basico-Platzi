a43f6364d2e1db85b7d91dd66a988122c780ab36
marc1896 <marco123107@hotmail.com>
2022-08-21 00:57:37 -0500

    Se agrego una nueva clase SwitchStatement

---
+++ src/SwitchStatement.java
@@ -0,0 +1,17 @@
+public class SwitchStatement {
+    public static void main(String[] args) {
+        int opt  = 1;
+
+        switch (opt){
+            case 1:
+                System.out.println("Opcion 1");
+                break;
+            case 2:
+                System.out.println("Opcion 2");
+                break;
+            case 3:
+                System.out.println("Opcion 3");
+                break;
+        }
+    }
+}
